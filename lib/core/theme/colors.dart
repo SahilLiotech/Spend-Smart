6774dc8e6ab3f2aab6fa4916ac3afe714ea4cb16
sahil pathan <pathansahil1800@gmail.com>
2025-02-01 14:28:49 +0530

    Colors Defined

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primaryColor = Color(0XFF344CB7);
+  static const Color secondaryColor = Color(0xFFDCEBFF);
+  static const Color incomeColor = Color(0XFF08B14C);
+  static const Color expenseColor = Color(0XFFF87378);
+
+  static const Color whiteColor = Color(0XFFFFFFFF);
+  static const Color blackColor = Color(0XFF000000);
+
+  static const Color dashBoardColor = Color(0XFF314394);
+}
---
+++ lib/features/onboarding/presentation/cubit/onboarding_cubit_state.dart
@@ -0,0 +1,10 @@
+part of 'onboarding_cubit_cubit.dart';
+
+sealed class OnboardingCubitState extends Equatable {
+  const OnboardingCubitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class OnboardingCubitInitial extends OnboardingCubitState {}
