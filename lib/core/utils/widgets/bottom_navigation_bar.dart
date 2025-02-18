96e0a685b5f5a78488c792f44471d7d82745caa3
SahilLiotech <sahil.liotech@gmail.com>
2025-02-18 13:52:14 +0530

    Bottom Navigation Bar UI

---
+++ lib/core/utils/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spend_smart/core/utils/custom_colors.dart';
+import 'package:spend_smart/core/utils/widgets/custom_text_widget.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  final Function(int) onItemSelected;
+  final int selectedIndex;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.onItemSelected,
+    this.selectedIndex = 0,
+  });
+
+  @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      notchMargin: 8.0,
+      child: SizedBox(
+        height: 70,
+        child: Row(
+          spacing: 6,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildNavItem("assets/images/home_icon.svg", "Home", 0),
+            _buildNavItem(
+                "assets/images/transaction_icon.svg", "Transaction", 1),
+            const SizedBox(width: 50), // Space for FAB
+            _buildNavItem("assets/images/category_icon.svg", "Category", 3),
+            _buildNavItem("assets/images/budget_icon.svg", "Budget", 4),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(String icon, String label, int index) {
+    return GestureDetector(
+      onTap: () => widget.onItemSelected(index),
+      child: Column(
+        spacing: 4,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            icon,
+            colorFilter: widget.selectedIndex == index
+                ? ColorFilter.mode(CustomColors.primaryColor, BlendMode.srcIn)
+                : ColorFilter.mode(CustomColors.hintTextColor, BlendMode.srcIn),
+          ),
+          CustomText(
+            text: label,
+            fontSize: 12,
+            color: widget.selectedIndex == index
+                ? CustomColors.primaryColor
+                : CustomColors.hintTextColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screens/dashboard_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:spend_smart/config/routes/routes.dart';
+import 'package:spend_smart/core/utils/custom_colors.dart';
 import 'package:spend_smart/core/utils/string.dart';
+import 'package:spend_smart/core/utils/widgets/bottom_navigation_bar.dart';
 import 'package:spend_smart/core/utils/widgets/button_widget.dart';
 import 'package:spend_smart/core/utils/widgets/custom_toast.dart';
 import 'package:spend_smart/features/auth/presentation/bloc/login_bloc/login_bloc.dart';
 
-class DashBoardScreen extends StatelessWidget {
+class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({super.key});
+
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  int _currentIndex = 0;
+
+  void _onNavItemSelected(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColors.secondaryColor,
       body: Center(
         child: BlocConsumer<LoginBloc, LoginState>(
           listener: (context, state) {
@@ -26,14 +43,29 @@ class DashBoardScreen extends StatelessWidget {
           },
           builder: (context, state) {
             return ButtonWidget(
-                onTap: () {
-                  context.read<LoginBloc>().add(LogoutEvent());
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, Routes.login, (route) => false);
-                },
-                buttonText: "Log Out");
+              onTap: () {
+                context.read<LoginBloc>().add(LogoutEvent());
+                Navigator.pushNamedAndRemoveUntil(
+                    context, Routes.login, (route) => false);
+              },
+              buttonText: "Log Out",
+            );
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        onPressed: () {
+          _onNavItemSelected(2);
+        },
+        child: SvgPicture.asset(
+          "assets/images/add_icon.svg",
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: _onNavItemSelected,
       ),
     );
   }
